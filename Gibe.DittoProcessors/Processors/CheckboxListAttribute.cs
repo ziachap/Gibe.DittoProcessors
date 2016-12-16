a4d6baa10385a516fe5f103767af7732da3eee03
Zia Siddique <zia.siddique@gibedigital.com>
2016-12-16 11:26:05 +0000

    Checkbox list processor

---
+++ Gibe.DittoProcessors/Processors/CheckboxListAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Gibe.DittoProcessors.Processors
+{
+	public class CheckboxListAttribute : TestableDittoProcessorAttribute
+	{
+		public override object ProcessValue()
+		{
+			if (string.IsNullOrEmpty(Value.ToString())) return Enumerable.Empty<string>();
+			return Value.ToString().Split(',');
+		}
+	}
+}
