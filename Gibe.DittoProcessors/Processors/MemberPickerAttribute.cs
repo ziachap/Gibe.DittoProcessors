a5e1cf77f66286741b1ff9840b44d5871fd562a4
Zia Siddique <zia.siddique@gibedigital.com>
2016-12-16 13:53:26 +0000

    Member picker attribute

---
+++ Gibe.DittoProcessors/Processors/MemberPickerAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Gibe.DittoProcessors.Processors.Models;
+using Gibe.DittoServices.ModelConverters;
+using Gibe.UmbracoWrappers;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace Gibe.DittoProcessors.Processors
+{
+	public class MemberPickerAttribute : TestableDittoProcessorAttribute
+	{
+		private readonly IModelConverter _modelConverter;
+
+		public MemberPickerAttribute(IModelConverter modelConverter)
+		{
+			_modelConverter = modelConverter;
+		}
+
+		public MemberPickerAttribute()
+		{
+			_modelConverter = DependencyResolver.Current.GetService<IModelConverter>();
+		}
+
+		public override object ProcessValue()
+		{
+			if (string.IsNullOrEmpty(Value.ToString())) return Enumerable.Empty<IPublishedContent>();
+			//TODO: Make TypedMember() in IUmbracoWrapper and use that
+			return Value.ToString()
+				.Split(',')
+				.Select(int.Parse)
+				.Select(new Umbraco.Web.Security.MembershipHelper(UmbracoContext.Current).GetById)
+				.Select(x => _modelConverter.ToModel<MemberModel>(x))
+				.ToList();
+		}
+	}
+}
---
+++ Gibe.DittoProcessors/Processors/Models/MemberModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Our.Umbraco.Ditto;
+
+namespace Gibe.DittoProcessors.Processors.Models
+{
+	public class MemberModel
+	{
+		[UmbracoProperty("name")]
+		public string Name { get; set; }
+
+		[UmbracoProperty("id")]
+		public int Id { get; set; }
+
+		[UmbracoProperty("umbracoMemberComments")]
+		public string Comments { get; set; }
+
+		[UmbracoProperty("umbracoMemberFailedPasswordAttempts")]
+		public string FailedPasswordAttempts { get; set; }
+
+		[UmbracoProperty("umbracoMemberApproved")]
+		public string IsApproved { get; set; }
+
+		[UmbracoProperty("umbracoMemberLockedOut")]
+		public string IsLockedOut { get; set; }
+
+		[UmbracoProperty("umbracoMemberLastLockoutDate")]
+		public DateTime LastLockoutDate { get; set; }
+
+		[UmbracoProperty("umbracoMemberLastLogin")]
+		public DateTime LastLoginDate { get; set; }
+
+		[UmbracoProperty("umbracoMemberLastPasswordChangeDate")]
+		public DateTime LastPasswordChangeDate { get; set; }
+	}
+}
