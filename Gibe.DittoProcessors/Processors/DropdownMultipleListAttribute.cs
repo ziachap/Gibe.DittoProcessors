370b8784d3b6e5b805984e7cb0dc8f9434a46f4a
Zia Siddique <zia.siddique@gibedigital.com>
2016-12-16 11:37:21 +0000

    Dropdown multiple list attribute

---
+++ Gibe.DittoProcessors/Processors/DropdownMultipleListAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Gibe.DittoProcessors.Processors
+{
+	public class DropdownMultipleListAttribute : TestableDittoProcessorAttribute
+	{
+		public override object ProcessValue()
+		{
+			if (string.IsNullOrEmpty(Value.ToString())) return Enumerable.Empty<string>();
+			return Value.ToString().Split(',');
+		}
+	}
+}
