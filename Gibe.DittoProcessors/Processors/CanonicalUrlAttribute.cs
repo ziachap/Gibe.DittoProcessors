1046d662bc17a154167797d94d865494c1901d89
Zia Siddique <zia.siddique@gibedigital.com>
2016-12-20 16:28:29 +0000

    Implement navigation attribute, canonical url attribute

---
+++ Gibe.DittoProcessors/Processors/CanonicalUrlAttribute.cs
@@ -0,0 +1,9 @@
+﻿using Umbraco.Web;
+
+namespace Gibe.DittoProcessors.Processors
+{
+	public class CanonicalUrlAttribute : TestableDittoProcessorAttribute
+	{
+		public override object ProcessValue() => Context.Content.UrlAbsolute();
+	}
+}
---
+++ Gibe.DittoProcessors/Processors/NavigationAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Gibe.Navigation;
+using Gibe.Navigation.Models;
+
+namespace Gibe.DittoProcessors.Processors
+{
+	public class NavigationAttribute : TestableDittoProcessorAttribute
+	{
+		private readonly INavigationService<INavigationElement> _navigationService;
+
+		public NavigationAttribute(INavigationService<INavigationElement> navigationService)
+		{
+			_navigationService = navigationService;
+		}
+
+		public NavigationAttribute()
+		{
+			_navigationService = DependencyResolver.Current.GetService<INavigationService<INavigationElement>>();
+		}
+
+		public override object ProcessValue()
+			=> _navigationService.GetNavigation(Context.Content.Url);
+	}
+}
