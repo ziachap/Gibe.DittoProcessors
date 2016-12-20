ac4d8cd098f1d51eecf9f04a7fc3be5c2147cd68
Zia Siddique <zia.siddique@gibedigital.com>
2016-12-20 16:11:07 +0000

    Implement next and previous url attribute, Create DittoProcessors test project

---
+++ Gibe.DittoProcessors/Processors/NextUrlAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gibe.DittoProcessors.Processors
+{
+	public class NextUrlAttribute : TestableDittoProcessorAttribute
+	{
+		private readonly HttpContextBase _httpContextBase;
+		private readonly string _pageAlias;
+
+		public NextUrlAttribute(HttpContextBase httpContextBase, string pageAlias = "page")
+		{
+			_httpContextBase = httpContextBase;
+			_pageAlias = pageAlias;
+		}
+
+		public NextUrlAttribute(string pageAlias = "page")
+		{
+			_httpContextBase = DependencyResolver.Current.GetService<HttpContextBase>();
+			_pageAlias = pageAlias;
+		}
+
+		public override object ProcessValue()
+		{
+			var canonicalUrl = _httpContextBase.Request.Url?.Scheme + "://"
+			                   + _httpContextBase.Request.Url?.Host
+			                   + _httpContextBase.Request.Url?.AbsolutePath;
+
+			var queryParameters = _httpContextBase.Request.Url.ParseQueryString();
+
+			if (queryParameters.AllKeys.Contains(_pageAlias))
+			{
+				var nextPage = int.Parse(queryParameters[_pageAlias]) + 1;
+				queryParameters[_pageAlias] = nextPage.ToString();
+				return canonicalUrl + "?" + queryParameters;
+			}
+
+			queryParameters[_pageAlias] = 2.ToString();
+			return canonicalUrl + "?" + queryParameters;
+		}
+	}
+}
---
+++ Gibe.DittoProcessors/Processors/PreviousUrlAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gibe.DittoProcessors.Processors
+{
+	public class PreviousUrlAttribute : TestableDittoProcessorAttribute
+	{
+		private readonly HttpContextBase _httpContextBase;
+		private readonly string _pageAlias;
+
+		public PreviousUrlAttribute(HttpContextBase httpContextBase, string pageAlias = "page")
+		{
+			_httpContextBase = httpContextBase;
+			_pageAlias = pageAlias;
+		}
+
+		public PreviousUrlAttribute(string pageAlias = "page")
+		{
+			_httpContextBase = DependencyResolver.Current.GetService<HttpContextBase>();
+			_pageAlias = pageAlias;
+		}
+
+		public override object ProcessValue()
+		{
+			var canonicalUrl = _httpContextBase.Request.Url?.Scheme + "://"
+			                   + _httpContextBase.Request.Url?.Host
+			                   + _httpContextBase.Request.Url?.AbsolutePath;
+
+			var queryParameters = _httpContextBase.Request.Url.ParseQueryString();
+
+			if (queryParameters.AllKeys.Contains(_pageAlias))
+			{
+				var nextPage = int.Parse(queryParameters[_pageAlias]) - 1;
+				if (nextPage <= 0) return string.Empty;
+				queryParameters[_pageAlias] = nextPage.ToString();
+				return canonicalUrl + "?" + queryParameters;
+			}
+
+			return string.Empty;
+		}
+	}
+}
